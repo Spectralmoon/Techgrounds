b0e1411de382f6725a648bacc40f6447bb829090
Susana Barrero <muluc33@gmail.com>
2021-12-09 18:31:07 +0100

    modal

---
+++ JavaScript/functions/dom.js
@@ -0,0 +1,10 @@
+const body = document.body;
+// body.append("Hello World");
+
+// const div = document.createElement("div");
+
+// div.innerText = "Hello Word";
+// div.textContent = "Hello World2";
+// body.append(div);
+
+document.querySelector("div");
